41a2985b71699d521afcd0ff84449920a0e3d6f4
mirza-zain <mirzazain269@gmail.com>
2025-02-09 13:38:13 +0500

    Last Change

--- api/index.js
+++ api/index.js
@@ -15,11 +15,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // Serve static files (like CSS & JS) properly
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '.public')));
 
 // Serve HTML file
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "../public", "index.html"));
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // Routes
