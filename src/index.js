63ef48b55ad99cb22ce3602e54e5af4c9ed56ae4
mirza-zain <mirzazain269@gmail.com>
2025-02-09 12:43:15 +0500

    Trying deployment 2

--- src/index.js
+++ src/index.js
@@ -1,33 +1,32 @@
-import express from "express"
-import path, { dirname } from "path"
-import { fileURLToPath } from "url"
-import authRoutes from "./routes/authRoutes.js"
-import todoRoutes from "./routes/todoRoutes.js"
-import authMiddleware from "./middleware/authMiddleware..js"
+import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import authRoutes from "./routes/authRoutes.js";
+import todoRoutes from "./routes/todoRoutes.js";
+import authMiddleware from "./middleware/authMiddleware.js";
 
-const app = express()
-const PORT = process.env.PORT 
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Get the file path from the URL of the current module
-const __filename = fileURLToPath(import.meta.url)
+const __filename = fileURLToPath(import.meta.url);
 // Get the directory name from the file path
-const __dirname = dirname(__filename)
+const __dirname = dirname(__filename);
 
-//Middleware
-app.use(express.json())
+// Middleware
+app.use(express.json());
 // Serves the HTML file from the /public directory
-//Tells express to serve all files from the public folder as static assests /files. Any requests for the css files will be resolved to the public directory.
-app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Serving up the HTM fies from the /public directory
+// Serving up the HTML files from the /public directory
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"))
-})
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 // Routes
-app.use('/auth', authRoutes)
-app.use('/todos', authMiddleware, todoRoutes)
+app.use('/auth', authRoutes);
+app.use('/todos', authMiddleware, todoRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server has started on PORT ${PORT}`)
-})
+    console.log(`Server has started on PORT ${PORT}`);
+});
